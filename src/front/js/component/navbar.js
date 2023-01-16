ab1701fe4540a91a6d20e894672e51c2041e227a
plopezrodrigo <patrylr84@gmail.com>
2023-01-16 18:23:46 +0000

    Login estructura

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,64 +1,98 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import imagen from "../../img/logo.png";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+  const { store, actions } = useContext(Context);
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1">
-            {" "}
-            <img
-              src={imagen}
-              className="card-img-top"
-              alt=""
-              width="120"
-              height="120"
-            />
-          </span>
-        </Link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/about">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Quienes Somos
-                </a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact">
-                <a className="nav-link active" href="#">
-                  Contacto
-                </a>
-              </Link>
-            </li>
-          </ul>
-          <form>
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Buscar"
-              aria-label="Buscar"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              <i className="far fa-search" />
-            </button>
-          </form>
+    <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <Link to="/">
+            <span className="navbar-brand mb-0 h1">
+              {" "}
+              <img
+                src={imagen}
+                className="card-img-top"
+                alt=""
+                width="120"
+                height="120"
+              />
+            </span>
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/about">
+                  <a
+                    className="nav-link active"
+                    aria-current="page"
+                    href="#"
+                    id="navbarSupportedContent"
+                  >
+                    Quienes Somos
+                  </a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact">
+                  <a
+                    className="nav-link active"
+                    href="#"
+                    id="navbarSupportedContent"
+                  >
+                    Contacto
+                  </a>
+                </Link>
+              </li>
+              <form>
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Buscar"
+                  aria-label="Buscar"
+                />
+                <button
+                  className="btn btn-outline-success"
+                  type="submit"
+                  id="iconbutton"
+                >
+                  <i className="far fa-search" />
+                </button>
+              </form>
+            </ul>
+            <div className="ml-auto">
+              {!store.token ? (
+                <Link to="/login">
+                  <button className="btn btn-primary" id="button">
+                    Log in
+                  </button>
+                </Link>
+              ) : (
+                <button
+                  onClick={() => actions.logout()}
+                  className="btn btn-primary"
+                  id="button"
+                >
+                  Log out
+                </button>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,6 +9,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import Login  from "./pages/login";
 
 //create your first component
 const Layout = () => {
@@ -25,6 +26,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<About />} path="/about" />
                         <Route element={<Contact />} path="/contact" />
+                        <Route element={<Login />} path="/login" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
