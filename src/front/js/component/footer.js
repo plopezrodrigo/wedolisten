7e959d9df639fe0297883207057e6836ba2d561e
plopezrodrigo <patrylr84@gmail.com>
2023-01-26 15:20:47 +0000

    arreglos footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,11 +4,11 @@ import imagen from "../../img/logo.png";
 
 export const Footer = () => (
   <footer>
-    <div className="container fluid">
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-        <div className="col mb-3">
-          <Link to="/">
-            <span className="navbar-brand mb-0 h1">
+    <div className="container fluid py-3 mt-3">
+      <div className="row py-5 my-5 border-top">
+        <div className="col-lg-3 mb-3">
+          <div className="col-lg-6 mb-6">
+            <Link to="/">
               {" "}
               <img
                 src={imagen}
@@ -17,15 +17,15 @@ export const Footer = () => (
                 width="120"
                 height="120"
               />
-            </span>
-          </Link>
-          <p className="text-muted">
+            </Link>
+          </div>
+          <p className="list-unstyled small text-muted">
             Baby Friendly© te ayuda a dar tu opinión sobre tus locales favoritos
             y que éstas puedan ser consultadas por otros usuarios
           </p>
         </div>
-        <div className="col mb-3"></div>
-        <div className="col mb-3">
+        <div className="col-6 col-lg-2 offset-lg-1 mb-3">
+          <h5>Enlaces</h5>
           <ul className="nav flex-column">
             <li className="nav-item mb-2">
               <a href="#" className="nav-link p-0 text-muted">
@@ -34,22 +34,23 @@ export const Footer = () => (
             </li>
             <li className="nav-item mb-2">
               <a href="#" className="nav-link p-0 text-muted">
-                About
+                Quienes somos
               </a>
             </li>
             <li className="nav-item mb-2">
               <a href="#" className="nav-link p-0 text-muted">
-                Contact
+                Contacto
               </a>
             </li>
             <li className="nav-item mb-2">
               <a href="#" className="nav-link p-0 text-muted">
-                FAQs
+                Gestores
               </a>
             </li>
           </ul>
         </div>
-        <div className="col mb-3">
+        <div className="col-6 col-lg-2 offset-lg-1 mb-3">
+          <h5>Legal</h5>
           <ul className="nav flex-column">
             <li className="nav-item mb-2">
               <a href="#" className="nav-link p-0 text-muted">
@@ -63,39 +64,31 @@ export const Footer = () => (
             </li>
             <li className="nav-item mb-2">
               <a href="#" className="nav-link p-0 text-muted">
-                Gestores
+                FAQs
               </a>
             </li>
           </ul>
         </div>
       </div>
       <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-
-          <p>© 2023 Company, Inc. Reservados todos los derechos.</p>
-          <ul className="list-unstyled d-flex">
-            <li className="ms-3">
-              <a className="link-dark" href="#">
-                <svg className="bi" width="24" height="24">
-                  <use xlinkHref="#twitter"></use>
-                </svg>
-              </a>
-            </li>
-            <li className="ms-3">
-              <a className="link-dark" href="#">
-                <svg className="bi" width="24" height="24">
-                  <use xlinkHref="#instagram"></use>
-                </svg>
-              </a>
-            </li>
-            <li className="ms-3">
-              <a className="link-dark" href="#">
-                <svg className="bi" width="24" height="24">
-                  <use xlinkHref="#facebook"></use>
-                </svg>
-              </a>
-            </li>
-          </ul>
-        </div>
+        <p>© 2023 Company, Inc. Reservados todos los derechos.</p>
+        <ul className="list-unstyled d-flex">
+          <li className="ms-3">
+            <a className="link-dark" href="#" id="iconbutton">
+              <i className="fab fa-facebook"></i>
+            </a>
+          </li>
+          <li className="ms-3">
+            <a className="link-dark" href="#" id="iconbutton">
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li className="ms-3">
+            <a className="link-dark" href="#" id="iconbutton">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   </footer>
