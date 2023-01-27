c56086718540c293ef41a21cf622166aefe88c2b
plopezrodrigo <patrylr84@gmail.com>
2023-01-27 13:59:33 +0000

    cambios estructura formulario

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -26,33 +26,56 @@ const Login = () => {
   };
     
   return (
-    <div className="container fluid align-center">
-      <div className="form-body">
-        <div className="row">
-          <h1>Hola de nuevo!</h1>
-          <h5>Bienvenido de nuevo a tu app.</h5>
-          {store.token && store.token != "" && store.token != undefined ? (
-            "You are logged in with this token" + store.token
-          ) : (
-            <div className="col-md-12">
-              <input
-                type="text"
-                placeholder="email"
-                name= "email"
-                onChange={handleChange}
-              />
-
-              <input
-                type="password"
-                placeholder="password"
-                name= "password"
-                onChange={handleChange}
-              />
-              <button onClick={handleClick} id="button">
-                Login
-              </button>
+    <div className="vh-100 gradient-custom">
+      <div className="container h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div className="card">
+            <div class="card-body text-center">
+              <h1>Hola de nuevo!</h1>
+              <h5>Bienvenido de nuevo a tu app.</h5>
             </div>
-          )}
+            <div className="form-outline">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                  {store.token && store.token != "" && store.token != undefined ? (
+                    "You are logged in with this token" + store.token
+                  ) : (
+                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                      <label class="form-label" for="typeEmailX">Email</label>
+                      <input
+                        type="text"
+                        id="typeEmailX-2"
+                        class="form-control mb-2"
+                        name= "email"
+                        onChange={handleChange}
+                      />
+                      <label class="form-label" for="typeEmailX">Contraseña</label>
+                      <input
+                        type="password"
+                        id="typePasswordX-2"
+                        name= "password"
+                        class="form-control mb-2"
+                        onChange={handleChange}
+                      />
+                      <p class="small mt-2">
+                        <a href="#!">¿Has olvidado tu contraseña?</a>
+                      </p>
+                      <button 
+                        className="btn-lg px-5 mb-3 mt-3"
+                        onClick={handleClick} id="button">
+                        Login
+                      </button>
+                    </div>
+                  )}
+                  <div>
+                  <p class="ms-3 me-3 mb-3 text-center">¿No tienes una cuenta? 
+                    <a href="#!" class="fw-bold">Registrate</a>
+                  </p>
+                </div>
+                </div>
+            </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
