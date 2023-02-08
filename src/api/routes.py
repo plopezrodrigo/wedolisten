8cb9cdac25b22fc2198f1b3143af31c4938ba400
plopezrodrigo <patrylr84@gmail.com>
2023-02-08 16:41:48 +0000

    delete fav con errores en commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -140,6 +140,18 @@ def add_Favourit(id):
         db.session.commit()
     return jsonify(favourit.serialize()), 200
 
+@api.route('/deletefavourit/<id>', methods=['DELETE'])
+@jwt_required()
+def delete_Favourit(id):
+    try:
+        favourit = Favourit.query.filter_by(id=id).first()
+        print(favourit)
+        db.session.delete(favourit)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"mensaje": str(e)}), 400
+    return jsonify({"mensaje":"ok"}), 200
+
 ''' 
 @api.route("/User/<id>", methods=["DELETE"])
 def Users_delete(id):
