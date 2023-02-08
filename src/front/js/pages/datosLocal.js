ebc985c7af38055ee468df65960f4243165e6078
lavp999 <lavp99@hotmail.com>
2023-02-08 14:37:10 +0000

    con Jose MAnuel

--- src/front/js/pages/datosLocal.js
+++ src/front/js/pages/datosLocal.js
@@ -3,134 +3,260 @@ import { useNavigate, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
-
 export const DatosLocal = () => {
-	const params = useParams()
-	const [local, setLocal] = useState({})
-	const [formData, setFormData] = useState({"name"            : local.name,
-											  "address"         : local.address,
-											  "url"             : local.url,
-											  "image_url"       : local.image_url,
-											  "telf"            : local.telf,
-											  "email"           : local.email,
-											  "location"        : local.location,
-											  "description"     : local.description,
-											  "cambiador"       : local.cambiador,
-											  "trona"           : local.trona,
-											  "accessible_carrito" : local.accessible_carrito,
-											  "espacio_carrito"    : local.espacio_carrito,
-											  "ascensor"           : local.ascensor,
-											  "productos_higiene"  : local.productos_higiene
-									});
-	const [mensaje, setMensaje] = useState(null); 
-	const navigate = useNavigate();
-	const { store, actions } = useContext(Context);
-  
-    const miUseEffect = async () => {
-        const resp = await fetch(`${process.env.BACKEND_URL}/api/comercial-place/${params.local_id}`)
+  const params = useParams();
+  const [local, setLocal] = useState({});
+  const [mensaje, setMensaje] = useState(null);
+  const navigate = useNavigate();
+  const { store, actions } = useContext(Context);
 
-        if (resp.ok) return await resp.json();
-        else         return setMensaje(await resp.json());  
+  const miUseEffect = async () => {
+    const resp = await fetch(
+      `${process.env.BACKEND_URL}/api/comercial-place/${params.local_id}`
+    );
+
+    if (resp.ok) return await resp.json();
+    else return setMensaje(await resp.json());
+  };
+
+  useEffect(() => {
+    if (store.token && store.token != "" && store.token != undefined) {
+      navigate.push("/login");
     }
 
-    useEffect (()=> {
-		if (store.token && store.token != "" && store.token != undefined) {
-			navigate.push("/login");  
-		}
+    miUseEffect().then((resp) => setLocal(resp));
+  }, []);
 
-        miUseEffect().then(resp => setLocal(resp));
-    }, [])
+  const handleChange = (evento) => {
+    setLocal({ ...local, [evento.target.name]: evento.target.checked });
+  };
 
-	const handleChange = (evento) =>{
-		setFormData({...formData, [evento.target.name]: evento.target.value});
-	}
+  const handleChangecheck = (evento) => {
+	console.log(evento.target.checked);
+	setLocal({...local, [evento.target.name]: local[evento.target.name] ? false : true})
+    // console.log(local[evento.target.name]);
+  };
 
-	const handleChangecheck = (evento) =>{
-		local[evento.target.name] = (local[evento.target.name] ? false : true)
-	}
+  const handleSubmit = async (evento) => {
+    evento.preventDefault(); // para evitar la recarga ya que cancela el evento
+    console.log("Comercial_Place actualizar", local);
 
-	const handleSubmit = async (evento)=>{
-		evento.preventDefault(); // para evitar la recarga ya que cancela el evento
-		console.log("Comercial_Place actualizar", formData);
+    const resp = await fetch(
+      process.env.BACKEND_URL + "/api/Comercial_Place/" + params.local_id,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + sessionStorage.getItem("token"),
+        },
+        body: JSON.stringify(local),
+      }
+    );
 
-		const resp = await fetch(process.env.BACKEND_URL +"/api/Comercial_Place/"+ params.local_id, 
-			{method: 'POST',
-			headers:{"Content-Type": "application/json",
-					 "Authorization": 'Bearer '+ sessionStorage.getItem("token")
-					},
-			body: JSON.stringify(formData),
-			});
+    if (resp.ok) return navigate("/misLocales");
+    else return setMensaje(await resp.json());
+  };
 
-		if (resp.ok) return navigate("/misLocales");
-		else         return setMensaje(await resp.json());   
-	}
+  return (
+    <div className="container fluid align-center">
+      <div className="form-body">
+        <div className="row">
+          <h1 className="text-center">Registro</h1>
+          <h5 className="text-center">
+            Da de alta tus locales y mantenlos actualizados para recibir
+            opiniones de tus usuarios
+          </h5>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group py-3 m-0">
+                <img src={local.image_url} className="imagenLocal" alt="" />
+                <input
+                  type="text"
+                  name="image_url"
+                  defaultValue={local.image_url}
+                  required
+                  className="form-control imagenLocal"
+                  id="Inputimage_url"
+                  aria-describedby="image_urlHelp"
+                  placeholder="image url"
+                  onChange={handleChange}
+                />
+              </div>
 
-	return (
-		<div className="container fluid align-center"> 
-		  <div className="form-body"> 
-			<div className="row">
-			    <h1 className="text-center">Registro</h1>
-			    <h5 className="text-center">Da de alta tus locales y mantenlos actualizados para recibir opiniones de tus usuarios</h5>
-            </div>
-			<div className="row"> 
-				<div className="col-md-12">
-					<form onSubmit={handleSubmit}>
-						<div className="form-group py-3 m-0">
-							<img src={local.image_url} className="imagenLocal" alt="" />
-							<input type="text" name="image_url" defaultValue={local.image_url} required className="form-control imagenLocal" id="Inputimage_url" aria-describedby="image_urlHelp" placeholder="image url" onChange={handleChange} />
-						</div>
+              <div className="form-group">
+                <label htmlFor="InputEmail1">Nombre del local</label>
+                <input
+                  type="text"
+                  name="name"
+                  defaultValue={local.name}
+                  required
+                  className="form-control"
+                  id="InputName1"
+                  aria-describedby="nameHelp"
+                  placeholder="Nombre del local"
+                  onChange={handleChange}
+                />
+              </div>
+              <br />
+              <div className="form-group">
+                <h5>Descripción</h5>
+                <textarea
+                  name="description"
+                  defaultValue={local.description}
+                  required
+                  rows="3"
+                  cols="100"
+                  onChange={handleChange}
+                ></textarea>
+              </div>
+              <br />
 
-						<div className="form-group">
-							<label htmlFor="InputEmail1">Nombre del local</label>
-  							<input type="text" name="name" defaultValue={local.name} required className="form-control" id="InputName1" aria-describedby="nameHelp" placeholder="Nombre del local" onChange={handleChange} />
-						</div>
-                        <br/>
-						<div className="form-group">
-							<h5>Descripción</h5>
-							<textarea name="description" defaultValue={local.description} required rows="3" cols="100" onChange={handleChange} ></textarea>
-						</div>
-                        <br/>
+              <div className="form-group">
+                <label htmlFor="Inputurl1">Información de contacto</label>
+                <input
+                  type="text"
+                  name="url"
+                  className="form-control"
+                  id="Inputurl1"
+                  defaultValue={local.url}
+                  aria-describedby="urlHelp"
+                  placeholder="url"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  name="email"
+                  required
+                  className="form-control"
+                  id="InputEmail1"
+                  defaultValue={local.email}
+                  aria-describedby="emailHelp"
+                  placeholder="email"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  name="telf"
+                  required
+                  className="form-control"
+                  id="InputTelf1"
+                  defaultValue={local.telf}
+                  aria-describedby="TelfHelp"
+                  placeholder="Teléfono"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  name="address"
+                  required
+                  className="form-control"
+                  id="InputAddress1"
+                  defaultValue={local.address}
+                  ria-describedby="AddressHelp"
+                  placeholder="Dirección"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  name="location"
+                  className="form-control"
+                  id="InputLocation1"
+                  defaultValue={local.location}
+                  aria-describedby="locationHelp"
+                  placeholder="Localización"
+                  onChange={handleChange}
+                />
+              </div>
+              <br />
+              <div className="form-group">
+                <input
+                  type="checkbox"
+                  name="trona"
+                  className="form-check-input"
+                  id="InputTrona1"
+				  checked="false"
+				  onChange={handleChangecheck}
+                />
+                <label htmlFor="InputTrona1">trona</label>
+                <br />
 
-						<div className="form-group">
-							<label htmlFor="Inputurl1">Información de contacto</label>
-							<input type="text" name="url" className="form-control" id="Inputurl1" defaultValue={local.url} aria-describedby="urlHelp" placeholder="url" onChange={handleChange} />
-							<input type="text" name="email" required className="form-control" id="InputEmail1" defaultValue={local.email} aria-describedby="emailHelp" placeholder="email" onChange={handleChange} />
-							<input type="text" name="telf" required className="form-control" id="InputTelf1" defaultValue={local.telf} aria-describedby="TelfHelp" placeholder="Teléfono" onChange={handleChange} />
-							<input type="text" name="address" required className="form-control" id="InputAddress1" defaultValue={local.address} ria-describedby="AddressHelp" placeholder="Dirección" onChange={handleChange} />
-							<input type="text" name="location" className="form-control" id="InputLocation1" defaultValue={local.location} aria-describedby="locationHelp" placeholder="Localización" onChange={handleChange} />
-						</div>
-                        <br/>
-						<div className="form-group">
-							<input type="checkbox" name="trona" checked={(local.trona) ? "checked" : "" } className="form-check-input" id="InputTrona1" onChange={handleChangecheck} />
-							<label htmlFor="InputTrona1">trona</label><br/>
+                <input
+                  type="checkbox"
+                  name="cambiador"
+                  checked={local.cambiador ? "true" : "false"}
+                  className="form-check-input"
+                  id="InputCambiador2"
+                  onChange={handleChangecheck}
+                />
+                <label htmlFor="InputCambiador2">cambiador</label>
+                <br />
 
-                            <input type="checkbox" name="cambiador" checked={(local.cambiador) ? "checked" : "" } className="form-check-input" id="InputCambiador2"onChange={handleChangecheck} />
-                            <label htmlFor="InputCambiador2">cambiador</label><br/>
+                <input
+                  type="checkbox"
+                  name="accessible_carrito"
+                  checked={local.accessible_carrito ? "checked" : ""}
+                  className="form-check-input"
+                  id="InputAccessible_carrito3"
+                  onChange={handleChangecheck}
+                />
+                <label htmlFor="InputAccessible_carrito3">
+                  accessible_carrito
+                </label>
+                <br />
 
-                            <input type="checkbox" name="accessible_carrito" checked={(local.accessible_carrito) ? "checked" : "" } className="form-check-input" id="InputAccessible_carrito3" onChange={handleChangecheck} />
-                            <label htmlFor="InputAccessible_carrito3">accessible_carrito</label><br/>
+                <input
+                  type="checkbox"
+                  name="espacio_carrito"
+                  checked={local.espacio_carrito ? "checked" : ""}
+                  className="form-check-input"
+                  id="InputEspacio_carrito3"
+                  onChange={handleChangecheck}
+                />
+                <label htmlFor="InputEspacio_carrito3">espacio_carrito</label>
+                <br />
 
-                            <input type="checkbox" name="espacio_carrito" checked={(local.espacio_carrito) ? "checked" : "" } className="form-check-input" id="InputEspacio_carrito3" onChange={handleChangecheck} />
-                            <label htmlFor="InputEspacio_carrito3">espacio_carrito</label><br/>
+                <input
+                  type="checkbox"
+                  name="ascensor"
+                  checked={local.ascensor ? "checked" : ""}
+                  className="form-check-input"
+                  id="InputAscensor5"
+                  onChange={handleChangecheck}
+                />
+                <label htmlFor="InputAscensor5">ascensor</label>
+                <br />
 
-                            <input type="checkbox" name="ascensor" checked={(local.ascensor) ? "checked" : "" } className="form-check-input" id="InputAscensor5" onChange={handleChangecheck} />
-                            <label htmlFor="InputAscensor5">ascensor</label><br/>
+                <input
+                  type="checkbox"
+                  name="productos_higiene"
+                  checked={local.productos_higiene ? "checked" : ""}
+                  className="form-check-input"
+                  id="InputProductos_higiene5"
+                  onChange={handleChangecheck}
+                />
+                <label htmlFor="InputProductos_higiene5">
+                  productos_higiene
+                </label>
+                <br />
+              </div>
 
-                            <input type="checkbox" name="productos_higiene" checked={(local.productos_higiene) ? "checked" : "" } className="form-check-input" id="InputProductos_higiene5" onChange={handleChangecheck} />
-                            <label htmlFor="InputProductos_higiene5">productos_higiene</label><br/>
-						</div>
- 
-                        <br/>
-						{console.log("FormData antes ", formData)}
-						<div className="py-3 px-0 mx-0 d-flex justify-content-around">
-							<button type="submit" className="btn btn-primary" id="button">Guardar</button>
-							<a href="/misLocales" className="btn btn-primary" id="button">Volver</a>
-						</div>
-						{(mensaje != null) && <p>{mensaje}</p>}
-					</form>				  
-				</div>
-			</div>
-		  </div>
-		</div>
-	  );
+              <br />
+              {console.log("Local antes ", local)}
+              <div className="py-3 px-0 mx-0 d-flex justify-content-around">
+                <button type="submit" className="btn btn-primary" id="button">
+                  Guardar
+                </button>
+                <a href="/misLocales" className="btn btn-primary" id="button">
+                  Volver
+                </a>
+              </div>
+              {mensaje != null && <p>{mensaje}</p>}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
