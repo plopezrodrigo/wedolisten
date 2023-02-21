47dd144d8110914bf342e66ae9b17d3bcb39af60
plopezrodrigo <patrylr84@gmail.com>
2023-02-21 11:09:13 +0000

    klsdjhfk

--- src/front/js/component/localCard.js
+++ src/front/js/component/localCard.js
@@ -33,7 +33,7 @@ const LocalCard = (props) => {
         />
         <div className="card-body">
           <h6>
-            <Link to="/opinionUser/:id_local/:id_comment" className="card-title mb-1"><strong>{props.name}</strong>
+            <Link to={`/localDetail/${props.id}`} className="card-title mb-1"><strong>{props.name}</strong>
             <button id="iconbutton" onClick={()=>{add_favourites(props.id)}}> <i className="fas fa-heart"></i></button>
             </Link>
           </h6>
@@ -46,9 +46,7 @@ const LocalCard = (props) => {
             }
             {comentarios ? comentarios.length : 0} Opiniones
             </div>
-            <p className="mt-1" id="localcardtext">
-            Española, Contemporánea €€€€
-            </p>
+            <p className="mt-1" id="localcardtext">Española, Contemporánea €€€</p>
             <div className="mt-0">
             {/* <Link to="/opinionUser/:id_local/:id_comment">
               <button id="iconbutton"><i className="far fa-comment" /></button>
