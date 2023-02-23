148a41b708089494d0c999d26669fc057f4b3081
lavp999 <lavp99@hotmail.com>
2023-02-23 19:25:22 +0000

    a medias envio de emails

--- src/front/js/pages/contact.js
+++ src/front/js/pages/contact.js
@@ -1,11 +1,44 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState , useContext , useRef } from "react";
 import { Context } from "../store/appContext";
-
+import CustomModal from "../component/customModal";
+import { useModal } from "../hooks/UseModal";
+//import emailjs from '@emailjs/browser';
 
 export const Contact = (props) => {
+  const [mensaje, setMensaje] = useState(null); 
   const { store, actions } = useContext(Context);
-  const params = useParams();
+	const [formData, setFormData] = useState({});
+	const [isModalOpened, setIsModalOpened, toggleModal] = useModal(false);
+
+	const handleChange = (evento) =>{
+		setFormData({...formData, [evento.target.name]: evento.target.value});
+	}
+
+  const EnvioEmail = (datos) => {
+    const form = useRef();
+
+    console.log(".........", form.current);
+/*
+    emailjs.sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_ID, form.current, process.env.PUBLIC_KEY)
+    .then((result) => {
+        setMensaje(result.text);
+    }, (error) => {
+        setMensaje(error.text)
+        return false;
+    });
+*/    
+    return true;
+  };
+
+  const handleSubmit = (evento)=>{
+		evento.preventDefault(); // para evitar la recarga ya que cancela el evento
+
+    EnvioEmail(formData);
+    toggleModal();
+
+  }
+
+
 
   return (
     <div className="bg-contact3">
@@ -18,7 +51,7 @@ export const Contact = (props) => {
             <div className="col-12 col-md-8 col-lg-6 col-xl-5 ">
               <div className="card px-3" id="card">
                 <h3 className="mt-3" id="iconbutton">Escríbenos</h3>
-                  <form className="form-outline" noValidate>
+                  <form className="form-outline" noValidate  onSubmit={handleSubmit}>
                     <div className="col-md-12">
                       <label forhtml="basic-url" className="form-label alinear-izquierda mt-3">Nombre y Apellidos</label>
                       <input  className="form-control mb-2"
@@ -26,6 +59,7 @@ export const Contact = (props) => {
                               name="name"
                               defaultValue={ store.name}
                               required
+                              onChange={handleChange} 
                       />
                       <div className="valid-feedback">Campo nombre y apellidos es válido.</div>
                       <div className="invalid-feedback">Campo nombre y apellidos no puede estar en blanco.</div>
@@ -37,6 +71,7 @@ export const Contact = (props) => {
                               name="email"
                               defaultValue={ store.email }
                               required
+                              onChange={handleChange} 
                       />
                       <div className="valid-feedback">Campo Email es válido.</div>
                       <div className="invalid-feedback">Campo Email no puede estar en blanco.</div>
@@ -47,6 +82,7 @@ export const Contact = (props) => {
                               type="telefono"
                               name="Teléfono"
                               required
+                              onChange={handleChange} 
                       />
                       <div className="valid-feedback">Campo Teléfono es válido</div>
                       <div className="invalid-feedback">Campo Teléfono no puede estar en blanco</div>
@@ -69,6 +105,7 @@ export const Contact = (props) => {
                               type="text"
                               name="Comentarios"
                               required
+                              onChange={handleChange} 
                       />
                       <div className="valid-feedback">Campo Comentarios es válido</div>
                       <div className="invalid-feedback">Campo Comentarios no puede estar en blanco</div>
@@ -79,6 +116,7 @@ export const Contact = (props) => {
                               value=""
                               id="invalidCheck"
                               required
+                              onChange={handleChange} 
                       />
                       <label className="form-check-label"> Confirmo que he leido y acepto la Política de Privacidad y Aviso Legal.</label>
                       <div className="invalid-feedback">Por favor, confirma que has leido y aceptas la Política de Privacidad y Aviso Legal.</div>
@@ -94,5 +132,11 @@ export const Contact = (props) => {
           </div>
         </div>
       </div>
+			<CustomModal  show={isModalOpened}
+                    titulo="Gracias por tu comentario!!!"
+                    handleClose={() => {setIsModalOpened(false);}} // essto es por pruebas, en realidad no debería venir a modal si es porque no ha puesto puntuación
+			>
+					<div>{mensaje}</div>
+			</CustomModal>
     </div>
   );};
